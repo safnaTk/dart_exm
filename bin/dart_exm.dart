610c48a2f0cd9d4f1656205f8b2fb323c0de4f5d
safnaTk <safnatk697@gmail.com>
2025-01-29 12:24:17 +0530

    first commit

---
+++ bin/dart_exm.dart
@@ -0,0 +1,110 @@
+
+// 1.
+// void main() {
+//   printSquares(5);
+// }
+
+// void printSquares(int n) {
+//   for (int i = 
+//   1; i <= n; i++) {
+//     print(i * i);
+//   }
+// }
+
+
+// 2.
+
+
+// void main(){
+// Dog d=Dog();
+// Cat c=Cat();
+// d.makeSound();
+// c.makeSound();
+// }
+// abstract class Animal{
+//   void makeSound();
+
+// }
+
+// class Dog extends Animal{
+// @override
+// void makeSound() {
+//     print("woof");
+
+   
+//   }
+// }
+
+// class Cat extends Animal{
+// @override
+// void makeSound(){
+//   print("meow");
+// }
+// }
+
+
+// 3.
+
+
+void main(){
+  Circle c=Circle(5.0, "red");
+  c.displayColor();
+  print("radius:${c._radius}");
+
+Rectangle r=Rectangle(6.8, 3.7,"blue" );
+r.displayColor();
+print("length:${r._length},width:${r._width}");
+
+}
+class Shape{
+  String _color;
+  Shape(this._color);
+  displayColor(){
+    print("color:$_color");
+  }
+  String get color=>_color;
+  set color(String value)=>_color=value;
+}
+
+
+class Circle extends Shape{
+  double _radius;
+  Circle( this._radius,super._color);
+double get radius=>_radius;
+set radius(double value)=>_radius=value;
+
+  }
+
+
+
+class Rectangle extends Shape{
+  double _length;
+  double _width;
+Rectangle(this._length,this._width,super._color);
+
+double get length => _length;
+set length(double value) => _length = value;
+
+  double get width => _width;
+  set width(double value) => _width = value;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// Create a base class named Shape with a private instance variable color. 
+//Implement amethod displayColor() within the Shape class to display the color. 
+//Next, create two derived classes, Circle and Rectangle, that inherit from the Shape class. 
+//The Circle class should have a private property radius, and the Rectangle class should haveprivate properties length and width. Implement constructors for each derived class toinitialize their respective properties. To demonstrate encapsulation, provide getter and setter methods for the private
+// variables (color, radius, length, and width) 
+
+// 3.
+
---
+++ test/dart_exm_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_exm/dart_exm.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
