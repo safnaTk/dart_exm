1d7a15a3d8618be7a1c59dbd132dcf283caffec1
safnaTk <safnatk697@gmail.com>
2025-01-29 12:39:35 +0530

    mixin added

--- bin/dart_exm.dart
+++ bin/dart_exm.dart
@@ -46,51 +46,54 @@
 // 3.
 
 
-void main(){
-  Circle c=Circle(5.0, "red");
-  c.displayColor();
-  print("radius:${c._radius}");
+// void main(){
+//   Circle c=Circle(5.0, "red");
+//   c.displayColor();
+//   print("radius:${c._radius}");
 
-Rectangle r=Rectangle(6.8, 3.7,"blue" );
-r.displayColor();
-print("length:${r._length},width:${r._width}");
+// Rectangle r=Rectangle(6.8, 3.7,"blue" );
+// r.displayColor();
+// print("length:${r._length},width:${r._width}");
 
-}
-class Shape{
-  String _color;
-  Shape(this._color);
-  displayColor(){
-    print("color:$_color");
-  }
-  String get color=>_color;
-  set color(String value)=>_color=value;
-}
-
-
-class Circle extends Shape{
-  double _radius;
-  Circle( this._radius,super._color);
-double get radius=>_radius;
-set radius(double value)=>_radius=value;
-
-  }
+// }
+// class Shape{
+//   String _color;
+//   Shape(this._color);
+//   displayColor(){
+//     print("color:$_color");
+//   }
+//   String get color=>_color;
+//   set color(String value)=>_color=value;
+// }
 
 
+// class Circle extends Shape{
+//   double _radius;
+//   Circle( this._radius,super._color);
+// double get radius=>_radius;
+// set radius(double value)=>_radius=value;
 
-class Rectangle extends Shape{
-  double _length;
-  double _width;
-Rectangle(this._length,this._width,super._color);
-
-double get length => _length;
-set length(double value) => _length = value;
-
-  double get width => _width;
-  set width(double value) => _width = value;
-}
+//   }
 
 
 
+// class Rectangle extends Shape{
+//   double _length;
+//   double _width;
+// Rectangle(this._length,this._width,super._color);
+
+// double get length => _length;
+// set length(double value) => _length = value;
+
+//   double get width => _width;
+//   set width(double value) => _width = value;
+// }
+
+
+// 4.)Define a mixin called Logger that has a method logMessage(String message) to print messages to the console. 
+// Create an abstract class Vehicle with abstract methods startEngine() andstopEngine().
+// Implement the Logger mixin in a class Car that extends Vehicle.
+// Implement the abstract methods to print log messages when the engine starts 
 
 
 
